9e5065aa4c349c4f433db9bbd2120bfe8e289f54
Demir Boraj <demirboraj97@gmail.com>
2022-07-06 14:31:35 +0200

    Working on exercise

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
 // Write your code here
+import './style.css'
+
+let state = {
+USState: '',
+breweries: [],
+}
+
+function render (){
+
+}
+
+function listenToSelectStateForm (){
+let formEl = document.querySelector('#select-state-form')
+formEl?.addEventListener('submit', (event) => {
+event.preventDefault()
+let stateEl = document.querySelector('#select-state')
+state.USState = stateEl?.value
+render()
+ 
+})
+}
+listenToSelectStateForm()
